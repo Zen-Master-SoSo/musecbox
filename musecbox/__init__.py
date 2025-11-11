756d684a7a19f6a57f2a4ae314c8ac69ce488195
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-11-11 18:38:32 +0800

    Bump version 0.5.0 -> 0.5.1

--- musecbox/__init__.py
+++ musecbox/__init__.py
@@ -77,7 +77,7 @@ from simple_carla.qt import CarlaQt
 from qt_extras import DevilBox
 from log_soso import StreamToLogger
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 APPLICATION_NAME		= 'MusecBox'
 APP_PATH				= dirname(realpath(__file__))
