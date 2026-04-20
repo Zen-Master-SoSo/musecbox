4bf5277933d379ff37d143bc24f04912922f6af7
Liyang <liyang@holly.home>
2026-04-20 11:41:36 +0800

    Add rich pretty printing and ability to pass multiple project files to mbx-project-info

--- musecbox/scripts/mbx_project_info.py
+++ musecbox/scripts/mbx_project_info.py
@@ -1,4 +1,4 @@
-#  musecbox/bin/mb-project-info.py
+#  musecbox/scripts/mbx_project_info.py
 #
 #  Copyright 2025 Leon Dionne <ldionne@dridesign.sh.cn>
 #
@@ -21,13 +21,24 @@
 Displays information about a saved MusecBox project.
 """
 import logging, argparse, sys, json
-from os.path import abspath
+from os import access, R_OK
+from os.path import dirname, abspath, join, exists
+from rich import print as rprint
 from musecbox import PROJECT_OPTION_KEYS, LOG_FORMAT
+
+def print_sfz(sfz_path):
+	print(sfz_path, end='')
+	if exists(sfz_path):
+		if access(sfz_path, R_OK):
+			print()
+		else:
+			rprint(' [red]\[not readable\][/red]')
+	else:
+		rprint(' [red]\[missing][/red]')
 
 def main():
 	p = argparse.ArgumentParser()
-	p.add_argument('Filename', type = str, nargs = '+',
-		help = 'MuseScore score to use for port setup, or saved port setup')
+	p.add_argument('Filename', type = str, nargs = '+', help = 'MusecBox project to display')
 	p.add_argument("--show-sfzs", "-s", action = "store_true")
 	p.add_argument("--show-channels", "-c", action = "store_true")
 	p.add_argument("--show-plugins", "-p", action = "store_true")
@@ -41,56 +52,72 @@ def main():
 	log_level = logging.DEBUG if options.verbose else logging.ERROR
 	logging.basicConfig(level = log_level, format = LOG_FORMAT)
 
-	try:
-		with open(options.Filename[0], 'r') as fh:
-			project_definition = json.load(fh)
-	except FileNotFoundError:
-		p.exit(f'"{options.Filename[0]}" is not a file')
-	except json.JSONDecodeError:
-		p.exit(f'There was an error decoding "{options.Filename[0]}"')
+	for filename in options.Filename:
+		try:
+			with open(filename, 'r') as fh:
+				project_definition = json.load(fh)
 
-	show_channels = options.show_channels or \
-		options.show_sfzs or \
-		options.show_plugins and any(trackdef["plugins"] \
-			for portdef in project_definition["ports"] \
-			for trackdef in portdef["tracks"])
-	show_shared_plugins = options.show_plugins and bool(project_definition["shared_plugins"])
-	show_options = options.show_options and bool(project_definition['options'])
-	show_section_heads = sum([show_channels, show_shared_plugins, show_options]) > 1
+		except FileNotFoundError:
+			rprint(f'{filename} [red]\[not a file][/red]')
 
-	if show_channels:
-		if show_section_heads:
-			print(' -- Channels --')
-		for portdef in project_definition["ports"]:
-			for trackdef in portdef["tracks"]:
-				sfz = abspath(trackdef["sfz"]) if options.abspath else trackdef["sfz"]
-				if options.show_channels or options.show_plugins and trackdef["plugins"]:
-					print(f' Port {portdef["port"]}\tChannel {trackdef["channel"]}\t' + \
-						f'{trackdef["instrument_name"]} ({trackdef["voice"]})\t{sfz}')
-					if options.show_plugins:
-						for saved_state in trackdef["plugins"]:
-							print(f'     Plugin {saved_state["vars"]["moniker"]}\t{saved_state["plugin_def"]["label"]}')
-				elif options.show_sfzs:
-					print(sfz)
-		if show_section_heads:
-			print()
-	if options.show_plugins and project_definition["shared_plugins"]:
-		if show_section_heads:
-			print(' -- Shared Plugins --')
-		for saved_state in project_definition["shared_plugins"]:
-			print(f' {saved_state["vars"]["moniker"]}\t{saved_state["plugin_def"]["label"]}')
-		if show_section_heads:
-			print()
-	if options.show_options and project_definition['options']:
-		if show_section_heads:
-			print(' -- Options --')
-		fmt = ' {0:%ds}: {1}' % max(len(key) for key in PROJECT_OPTION_KEYS)
-		for key in PROJECT_OPTION_KEYS:
-			if key in project_definition['options']:
-				print(fmt.format(key, project_definition['options'][key]))
+		except json.JSONDecodeError as e:
+			rprint(f'{filename} [red]\[JSON decode error: {e}][/red]')
+
+		else:
+
+			if len(options.Filename) > 1:
+				rprint(f'[bold black]{filename}[/bold black]')
+
+			show_channels = options.show_channels or \
+				options.show_sfzs or \
+				options.show_plugins and any(trackdef["plugins"] \
+					for portdef in project_definition["ports"] \
+					for trackdef in portdef["tracks"])
+			show_shared_plugins = options.show_plugins and bool(project_definition["shared_plugins"])
+			show_options = options.show_options and bool(project_definition['options'])
+			show_section_heads = sum([show_channels, show_shared_plugins, show_options]) > 1
+
+			if show_channels:
+
+				if show_section_heads:
+					rprint(' [bold green]-- Channels --[/bold green]')
+
+				for portdef in project_definition["ports"]:
+					for trackdef in portdef["tracks"]:
+						sfz_path = join(dirname(filename), trackdef["sfz"])
+						if options.show_channels or options.show_plugins and trackdef["plugins"]:
+							print(f' Port {portdef["port"]}', end='\t')
+							print(f'Channel {trackdef["channel"]}', end='\t')
+							print(f'{trackdef["instrument_name"]} ({trackdef["voice"]})', end='\t')
+						print_sfz(abspath(sfz_path) if options.abspath else sfz_path)
+						if options.show_plugins:
+							for saved_state in trackdef["plugins"]:
+								print(f'     Plugin {saved_state["vars"]["moniker"]}', end='\t')
+								print(saved_state["plugin_def"]["label"])
+				if show_section_heads:
+					print()
+
+			if options.show_plugins and project_definition["shared_plugins"]:
+				if show_section_heads:
+					rprint(' [bold blue]-- Shared Plugins --[/bold blue]')
+				for saved_state in project_definition["shared_plugins"]:
+					print(f' {saved_state["vars"]["moniker"]}\t{saved_state["plugin_def"]["label"]}')
+				if show_section_heads:
+					print()
+
+			if options.show_options and project_definition['options']:
+				if show_section_heads:
+					rprint(' [bold dark_orange]-- Options --[/bold dark_orange]')
+				fmt = ' {0:%ds}: {1}' % max(len(key) for key in PROJECT_OPTION_KEYS)
+				for key in PROJECT_OPTION_KEYS:
+					if key in project_definition['options']:
+						print(fmt.format(key, project_definition['options'][key]))
+
+			if len(options.Filename) > 1:
+				print()
 
 if __name__ == '__main__':
 	sys.exit(main() or 0)
 
 
-#  musecbox/bin/mb-project-info.py
+#  musecbox/scripts/mbx_project_info.py
