87da3d20850a869db8f8d420eb16d790a7111888
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-07-23 11:02:13 +0800

    Bump version 0.12.1 -> 0.13.0

--- musecbox/__init__.py
+++ musecbox/__init__.py
@@ -76,7 +76,7 @@ from simple_carla import (
 from simple_carla.qt import CarlaQt
 from qt_extras import DevilBox
 
-__version__ = "0.12.1"
+__version__ = "0.13.0"
 
 APPLICATION_NAME		= 'MusecBox'
 APP_PATH				= dirname(abspath(__file__))
