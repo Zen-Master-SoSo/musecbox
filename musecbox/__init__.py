2d5036a1774950e1b94aa37f091fc99771d736ab
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-08-08 07:22:26 +0800

    Bump version 0.14.2 -> 0.14.3

--- musecbox/__init__.py
+++ musecbox/__init__.py
@@ -77,7 +77,7 @@ from simple_carla.qt import CarlaQt
 from qt_extras import DevilBox
 from xdg_soso import XDGSetup, XDGMime
 
-__version__ = "0.14.2"
+__version__ = "0.14.3"
 
 VENDOR_NAME				= 'ZenSoSo'
 APPLICATION_NAME		= 'MusecBox'
